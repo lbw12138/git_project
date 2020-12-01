157169bdc04f8493077d671a30db7b199548238e
lbw12138 <504679496@qq.com>
2020-12-01 17:13:43 +0800

    提交到本次仓库

---
+++ Servers/src/student_prc/StudentModel.java
@@ -0,0 +1,12 @@
+package student_prc;
+
+public class StudentModel {
+	private int id;
+	private String love;
+	
+	public StudentModel(int id,String love) {
+	super();
+	this.id = id;
+	this.love = love;
+	}
+}
